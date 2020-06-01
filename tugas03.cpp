7ae50d85893245fb0999f77e416fb3d13a86e338
hasansuryaman <hasan.suryaman@outlook.com>
2020-06-01 19:41:46 +0700

    Tugas 3 C++

---
+++ tugas03.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int a, b, kali;
+    a = 10;
+    b = 20;
+    kali;
+    kali =  a * b;
+    cout <<"Nilai Variabel A : " <<a <<endl;
+    cout <<"Nilai Variabel B : " <<b <<endl;
+    cout <<"Hasil Perkalian Kedua Variabel Adalah : " <<kali <<endl;
+}
